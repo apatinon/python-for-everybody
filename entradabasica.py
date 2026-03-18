5d55ee8f6dade5097159b47ce1f68bba535f233b
Andrés Patiño <apatinon@unal.edu.co>
2026-03-17 22:18:38 -0500

    pedirinfo

---
+++ entradabasica.py
@@ -0,0 +1,8 @@
+nombre = input('ingrese su nombre: ')
+edad = input('ingrese su edad: ')
+altura = input('ingrese su altura en cm: ')
+edadfutura = int(edad)+5
+print ("Hola" , nombre)
+print ("Tienes" , edad , "años")
+print ("En 5 años tendrás" , edadfutura , "años")
+print ("Tu altura en cm es " , altura , "cm")
