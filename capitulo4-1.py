5dc792690a05f5ce9e94204fe5a9b1d0c8123198
Andrés Patiño <apatinon@unal.edu.co>
2026-03-24 13:16:33 -0500

    módulo 4 del curso python for everybody

---
+++ capitulo4-1.py
@@ -0,0 +1,9 @@
+#CAPITULO 4: FUNCIONES: ejemplos y ejercicios de esta sección
+
+#ejemplo simple mostrado en el curso
+#def addtwo(a, b):
+#    added = a + b
+#    return added
+#x = addtwo(3, 5)
+#print(x) 
+
