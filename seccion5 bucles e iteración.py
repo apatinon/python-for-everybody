e341f5fb81e56d4e07d3be71adda4036b852712d
Andrés Patiño <apatinon@unal.edu.co>
2026-03-24 16:13:34 -0500

    Ejemplos y ejercicio de la sección 5

--- seccion5 bucles e iteración.py
+++ seccion5 bucles e iteración.py
@@ -1,5 +1,12 @@
-n = float(input("ingrese el inicio de su cuenta regresiva: "))
-while n > 0:
-    print(n)
-    n = n-1
-print('BOOOOOOM!!')
+#primer ejemplo de esta sección que usa solo "while"
+#n = float(input("ingrese el inicio de su cuenta regresiva: "))
+#while n > 0:
+#    print(n)
+#    n = n-1
+#print('BOOOOOOM!!')
+
+# ahora haremos algo parecido a lo de arriba pero con "for" y "in"
+#for i in [5 , 4, 3, 2, 1]:
+#    print(i)
+#print('CATAPLASSSSS!!!')
+
